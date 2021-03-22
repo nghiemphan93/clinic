fa26c1bcd1896def59a22b007461a202adc39947
Nghiem Phan <nphan@app-pilots.de>
2021-03-22 22:20:45 +0100

    fixed tailwind intellisence and reduced bundle size for production

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-    prefix: '',
-    purge: {
-      content: [
-        './src/**/*.{html,ts}',
-      ]
-    },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+  prefix: "",
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 };

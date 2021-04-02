50ff346831a3757a01dc14a9df7d9d5447134645
Nghiem Phan <nphan@app-pilots.de>
2021-04-02 14:04:48 +0200

    implemented getUser, updateUser and deleteUser in UserService and UserController

--- backend/src/main/java/com/springbootjwtpostgres/backend/user/UserController.java
+++ backend/src/main/java/com/springbootjwtpostgres/backend/user/UserController.java
@@ -1,9 +1,12 @@
 package com.springbootjwtpostgres.backend.user;
 
+import javassist.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,8 +23,23 @@ public class UserController {
         return new ResponseEntity<>(this.userService.getUsers(userPage, userSearchCriteria), HttpStatus.OK);
     }
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable Long userId) throws NotFoundException {
+        return new ResponseEntity<>(this.userService.getUser(userId), HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return new ResponseEntity<>(this.userService.createUser(user), HttpStatus.OK);
+    }
+
+    @PutMapping("{userId}")
+    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody @Valid User newUser) throws NotFoundException {
+        return new ResponseEntity<>(this.userService.updateUser(userId, newUser), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{/userId}")
+    public void deleteUser(@PathVariable Long userId) {
+        this.userService.deleteUser(userId);
     }
 }
--- backend/src/main/java/com/springbootjwtpostgres/backend/user/UserService.java
+++ backend/src/main/java/com/springbootjwtpostgres/backend/user/UserService.java
@@ -1,5 +1,6 @@
 package com.springbootjwtpostgres.backend.user;
 
+import javassist.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,26 @@ public class UserService {
         return this.userCriteriaRepo.findAllWithFilters(userPage, userSearchCriteria);
     }
 
+    public User getUser(Long userId) throws NotFoundException {
+        return this.userRepo.findById(userId)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+    }
+
     public User createUser(User user) {
         return this.userRepo.save(user);
+    }
+
+    public User updateUser(Long userId, User newUser) throws NotFoundException {
+        User oldUser = this.userRepo.findById(userId)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+        oldUser.setEmail(newUser.getEmail());
+        oldUser.setUsername(newUser.getUsername());
+        oldUser.setPassword(newUser.getPassword());
+        oldUser.setRoles(newUser.getRoles());
+        return this.userRepo.save(oldUser);
+    }
+
+    public void deleteUser(Long userId) {
+        this.userRepo.deleteById(userId);
     }
 }

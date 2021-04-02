695d935e0c2638c319435683a70d32a28175db54
Nghiem Phan <nphan@app-pilots.de>
2021-04-02 13:26:53 +0200

    added hanler for usernameNotFound

--- backend/src/main/java/com/springbootjwtpostgres/backend/exceptionhandlers/ExceptionHelper.java
+++ backend/src/main/java/com/springbootjwtpostgres/backend/exceptionhandlers/ExceptionHelper.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -17,41 +18,42 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ExceptionHelper extends ResponseEntityExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(ExceptionHelper.class);
 
-//    @org.springframework.web.bind.annotation.ExceptionHandler(value = {Unauthorized.class})
-//    public ResponseEntity<Object> handleUnauthorizedException(Unauthorized ex) {
-//        logger.error("Unauthorized Exception: " + ex.getMessage());
-//        return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-//    }
-
     public ResponseEntity<ExceptionResponse> buildResponseEntity(HttpStatus status, String message, String debugMessage) {
         return new ResponseEntity<>(new ExceptionResponse(status, message, debugMessage), status);
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public ResponseEntity<ExceptionResponse> handleUsernameNotFoundException(
+            UsernameNotFoundException ex, WebRequest request) {
+        logger.error(ex.getMessage());
+        return buildResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage(), ex.getLocalizedMessage());
     }
 
     @ExceptionHandler({AccessDeniedException.class})
     public ResponseEntity<ExceptionResponse> handleAccessDeniedException(
             AccessDeniedException ex, WebRequest request) {
-        System.out.println("not permitted....");
+        logger.error(ex.getMessage());
         return buildResponseEntity(HttpStatus.FORBIDDEN, ex.getMessage(), ex.getLocalizedMessage());
     }
 
     @ExceptionHandler({AuthenticationException.class})
     public ResponseEntity<ExceptionResponse> handleUnauthorizedException(
             AuthenticationException ex, WebRequest request) {
-        System.out.println("unauthorized....");
+        logger.error(ex.getMessage());
         return buildResponseEntity(HttpStatus.UNAUTHORIZED, ex.getMessage(), ex.getLocalizedMessage());
     }
 
     @ExceptionHandler({ApplicationException.class})
     public ResponseEntity<ExceptionResponse> handleNotFoundException(
             ApplicationException ex) {
-        System.out.println("not found....");
+        logger.error(ex.getMessage());
         return buildResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage(), ex.getLocalizedMessage());
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<ExceptionResponse> handleGeneralException(
             Exception ex) {
-        System.out.println("global exception...");
+        logger.error(ex.getMessage());
         return buildResponseEntity(HttpStatus.BAD_REQUEST, ex.getMessage(), ex.getLocalizedMessage());
     }
 }

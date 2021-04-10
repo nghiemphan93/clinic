26e37ffc752842dc04b0e8cc92dde7ff820dc99d
Nghiem Phan <nphan@app-pilots.de>
2021-04-10 02:46:18 +0200

    added SignUpPage

--- frontend/src/app/pages/auth/sign-up/sign-up.component.ts
+++ frontend/src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Observable } from 'rxjs';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ERole } from '../../../models/user/ERole';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { SignUpRequest } from '../../../models/user/SignUpRequest';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  validateForm!: FormGroup;
+  isAuth$ = new Observable<boolean>();
+  roles: Set<ERole> = new Set([
+    ERole.ROLE_DOCTOR,
+    ERole.ROLE_MANAGER,
+    ERole.ROLE_NURSE,
+  ]);
+  isLoading = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private messageService: NzMessageService
+  ) {}
 
   ngOnInit(): void {
+    this.isAuth$ = this.authService.getAuth();
+
+    this.validateForm = this.fb.group({
+      userName: new FormControl('string', [Validators.required]),
+      password: new FormControl('string', [Validators.required]),
+      email: new FormControl('string@string.com', [
+        Validators.required,
+        Validators.email,
+      ]),
+      roles: new FormControl([ERole.ROLE_MANAGER], [Validators.required]),
+    });
   }
 
+  async submitForm(): Promise<void> {
+    this.isLoading = true;
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+
+    const userName: string = this.validateForm.get('userName')?.value;
+    const password: string = this.validateForm.get('password')?.value;
+    const email: string = this.validateForm.get('email')?.value;
+    const roles: Set<ERole> = this.validateForm.get('roles')?.value;
+
+    const signUpRequest = new SignUpRequest(userName, email, roles, password);
+
+    try {
+      await this.authService.signUp(signUpRequest);
+      this.messageService.success('user created successfully!!!');
+      this.validateForm.reset();
+    } catch (e) {
+      this.messageService.error(e.message);
+    } finally {
+      this.isLoading = false;
+    }
+  }
 }
--- frontend/src/app/pages/auth/sign-up/sign-up.module.ts
+++ frontend/src/app/pages/auth/sign-up/sign-up.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SignUpRoutingModule } from './sign-up-routing.module';
-
+import { NgZorroAntdModule } from '../../../shared/ng-zorro-antd/ng-zorro-antd.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    SignUpRoutingModule
-  ]
+  imports: [CommonModule, SignUpRoutingModule],
 })
-export class SignUpModule { }
+export class SignUpModule {}

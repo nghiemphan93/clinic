d1733ba4f8d42845cbf2bce844cee5d51125c31f
Nghiem Phan <nphan@app-pilots.de>
2021-04-10 02:45:56 +0200

    init services in AppComponent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,8 +13,13 @@ import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { NZ_CONFIG, NzConfig } from 'ng-zorro-antd/core/config';
 
 registerLocaleData(vi);
+
+const ngZorroConfig: NzConfig = {
+  message: { nzDuration: 5000, nzPauseOnHover: true },
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,6 +37,7 @@ registerLocaleData(vi);
   providers: [
     { provide: NZ_I18N, useValue: vi_VN },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: NZ_CONFIG, useValue: ngZorroConfig },
   ],
   bootstrap: [AppComponent],
 })

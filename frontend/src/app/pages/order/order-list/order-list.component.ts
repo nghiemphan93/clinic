48821f778454f2b926b64bb940e43a4a231299d3
Nghiem Phan <nphan@app-pilots.de>
2021-04-19 20:58:48 +0200

    implemented OrderListPage

--- frontend/src/app/pages/order/order-list/order-list.component.ts
+++ frontend/src/app/pages/order/order-list/order-list.component.ts
@@ -1,15 +1,117 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { BasePage } from '../../../models/base/BasePage';
+import { NzTableQueryParams } from 'ng-zorro-antd/table';
+import { SortEDirection } from '../../../models/base/SortEDirection';
+import { OrderService } from '../../../services/order.service';
+import { OrderSearchCriteria } from '../../../models/order/OrderSearchCriteria';
+import { Order } from '../../../models/order/Order';
+import {
+  initColumnFilter,
+  OrderColumnFilter,
+} from '../../../models/order/OrderColumnFilter';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.scss']
+  styleUrls: ['./order-list.component.scss'],
 })
 export class OrderListComponent implements OnInit {
+  subscription = new Subscription();
+  total = 1;
+  pageSize = 10;
+  pageIndex = 1;
+  loading = true;
+  orders: Order[] = [];
 
-  constructor() { }
+  columnFilter: OrderColumnFilter = JSON.parse(
+    JSON.stringify(initColumnFilter)
+  );
 
-  ngOnInit(): void {
+  constructor(
+    private orderService: OrderService,
+    private messageService: NzMessageService
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
+  loadDataFromServer(page?: BasePage, searchCriteria?: OrderSearchCriteria) {
+    this.loading = true;
+    this.subscription.add(
+      this.orderService.getAll(page, searchCriteria).subscribe(
+        (data) => {
+          this.total = data.totalElements;
+          this.orders = data.content;
+          this.loading = false;
+        },
+        (e) => {
+          this.messageService.error(e.message);
+          this.loading = false;
+        }
+      )
+    );
+  }
+
+  onQueryParamsChange(params: NzTableQueryParams) {
+    const { pageSize, pageIndex, sort, filter } = params;
+    const currentSort = sort.find((item) => item.value !== null);
+    const sortBy = (currentSort && currentSort.key) || 'createdAt';
+    const sortDirection = (currentSort && currentSort.value) || 'ascend';
+
+    const page: BasePage = {
+      pageNumber: pageIndex - 1,
+      pageSize: pageSize,
+      sortDirection:
+        sortDirection === 'ascend' ? SortEDirection.ASC : SortEDirection.DESC,
+      sortBy: sortBy,
+    };
+
+    const searchCriteria: OrderSearchCriteria = this.getSearchCriteria();
+    this.loadDataFromServer(page, searchCriteria);
+  }
+
+  private getSearchCriteria(): OrderSearchCriteria {
+    const searchCriteria: OrderSearchCriteria = {
+      orderTypes: this.columnFilter.orderType.orderTypes
+        .filter((orderType) => orderType.checked === true)
+        .map((orderType) => orderType.value),
+      orderStatuses: this.columnFilter.orderStatus.orderStatuses
+        .filter((orderStatus) => orderStatus.checked === true)
+        .map((orderStatus) => orderStatus.value),
+      orderTotalPriceFrom: this.columnFilter.orderTotalPrice
+        .orderTotalPriceFrom,
+      orderTotalPriceTo: this.columnFilter.orderTotalPrice.orderTotalPriceTo,
+      createdAtFrom: this.columnFilter.createdAt.createdAtFrom,
+      createdAtTo: this.columnFilter.createdAt.createdAtTo,
+    };
+    return searchCriteria;
+  }
+
+  reset(): void {
+    this.resetAllColumns();
+    this.filterColumns();
+  }
+
+  private resetAllColumns() {
+    this.columnFilter = JSON.parse(JSON.stringify(initColumnFilter));
+  }
+
+  private setAllFiltersMenuInvisible() {
+    this.columnFilter.orderType.visible = false;
+    this.columnFilter.orderStatus.visible = false;
+    this.columnFilter.orderTotalPrice.visible = false;
+    this.columnFilter.createdAt.visible = false;
+  }
+
+  filterColumns() {
+    this.setAllFiltersMenuInvisible();
+    this.loadDataFromServer(new BasePage(), this.getSearchCriteria());
+  }
 }
--- frontend/src/app/pages/order/order.module.ts
+++ frontend/src/app/pages/order/order.module.ts
@@ -6,13 +6,44 @@ import { OrderEditComponent } from './order-edit/order-edit.component';
 import { OrderDetailComponent } from './order-detail/order-detail.component';
 import { OrderCreateComponent } from './order-create/order-create.component';
 import { OrderListComponent } from './order-list/order-list.component';
-
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 @NgModule({
-  declarations: [OrderEditComponent, OrderDetailComponent, OrderCreateComponent, OrderListComponent],
+  declarations: [
+    OrderEditComponent,
+    OrderDetailComponent,
+    OrderCreateComponent,
+    OrderListComponent,
+  ],
   imports: [
     CommonModule,
-    OrderRoutingModule
-  ]
+    OrderRoutingModule,
+    NzTypographyModule,
+    NzTableModule,
+    NzDropDownModule,
+    FormsModule,
+    NzButtonModule,
+    NzIconModule,
+    NzInputModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzSelectModule,
+    NzPopconfirmModule,
+    NzSpinModule,
+    NzDatePickerModule,
+    NzCheckboxModule,
+  ],
 })
-export class OrderModule { }
+export class OrderModule {}
